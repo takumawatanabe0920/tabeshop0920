039c7c553e3224bf669a64d91b9abfd3a229e5e1
渡辺拓馬 <>
2019-05-23 15:47:48 +0900

    aaa

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,9 +2,15 @@ class ShopsController < ApplicationController
   before_action :correct_user, only: [:destroy]
   before_action :require_user_logged_in
   def index
+    params[:q] = { starttime_eq: 1 }       if params[:starttime] == 1
+    params[:q] = { starttime_lteq: 6 }  if params[:starttime] == 2
+    params[:q] = { starttime_lteq: 12 }  if params[:starttime] == 3
+    params[:q] = { starttime_gteq: 18 } if params[:starttime] == 4
     @q = current_user.shops.ransack(params[:q])
     @shops = @q.result(distinct: true).recent.page(params[:page])
+    @shop = current_user.shops.all
   end
+
 
   def show
     @shop = current_user.shops.find(params[:id])
