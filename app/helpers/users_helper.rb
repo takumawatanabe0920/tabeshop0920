d4c1515d727d342ed20d37553e84b7f50079884f
渡辺拓馬 <>
2019-05-26 22:39:22 +0900

    aaa

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
   def gravatar_url(user, options = { size: 80 })
+    raise user.inspect
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
