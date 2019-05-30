9e57bfac81cfa6aee928ecbd19e09c88c337aea1
渡辺拓馬 <>
2019-05-30 22:00:17 +0900

    aaa

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,4 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !!current_user
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -66,8 +66,6 @@ ActiveRecord::Schema.define(version: 2019_05_27_061547) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
