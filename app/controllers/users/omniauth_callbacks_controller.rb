2226209e4d8f675bed1c5186aeefb3a99d721b45
渡辺拓馬 <>
2019-06-27 14:47:07 +0900

    aaa

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,20 +10,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def callback_from(provider)
-    provider = provider.to_s
+   provider = provider.to_s
 
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+   @user = User.find_for_oauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
-  end
+   if @user.persisted?
+     flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+     sign_in_and_redirect @user, event: :authentication
+   else
+     session["devise.#{provider}_data"] = request.env['omniauth.auth']
+     redirect_to new_user_registration_url
+   end
+ end
 
-  
+
 
   def basic_action
    @omniauth = request.env['omniauth.auth']
